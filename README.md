360b2aec6fc10cc36bcaf2c98697822490f4cd42
azhar-mohammed <mohd.azhar.moinuddin@gmail.com>
2018-02-15 14:23:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# customer-service-token-management
---
+++ src/test/java/com/abcbank/customerservicetokenmanagement/CustomerServiceTokenManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abcbank.customerservicetokenmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerServiceTokenManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
