913e321ca36490e657b1e435b743aea1eb6d98f8
azhar-mohammed <mohd.azhar.moinuddin@gmail.com>
2018-05-08 14:11:46 +0530

    Cosmetic change

--- src/main/java/com/abcbank/tokenmanage/consumer/RabbitMQConsumer.java
+++ src/main/java/com/abcbank/tokenmanage/consumer/RabbitMQConsumer.java
@@ -14,66 +14,70 @@ import com.abcbank.tokenmanage.counter.Receiver;
 
 public class RabbitMQConsumer {
 
-    private final ConnectionFactory connectionFactory;
+	private final ConnectionFactory connectionFactory;
 
-    /**
-     * The routing key is the message type that the consumer wants to read from the queue.
-     */
-    private String routingKey;
+	/**
+	 * The routing key is the message type that the consumer wants to read from the
+	 * queue.
+	 */
+	private String routingKey;
 
-    /**
-     * The queue name is the name of the queue that is shared between a pool of
-     * consumers for load balancing the messages within the same service.
-     */
-    private String queueName;
+	/**
+	 * The queue name is the name of the queue that is shared between a pool of
+	 * consumers for load balancing the messages within the same service.
+	 */
+	private String queueName;
 
-    private String name;
-    
-     public RabbitMQConsumer(String consumerName, String routingKey, String queueName, ConnectionFactory connectionFactory,Receiver receiver) {
-        this.name = consumerName;
-        this.routingKey = routingKey;
-        this.queueName = queueName;
-        this.connectionFactory = connectionFactory;
-      
-        initContainer(receiver);
-    }
+	private String name;
 
-    public String getRoutingKey() {
-        return routingKey;
-    }
+	public RabbitMQConsumer(String consumerName, String routingKey, String queueName,
+			ConnectionFactory connectionFactory, Receiver receiver) {
+		this.name = consumerName;
+		this.routingKey = routingKey;
+		this.queueName = queueName;
+		this.connectionFactory = connectionFactory;
 
-    public String getQueueName() {
-        return queueName;
-    }
+		initContainer(receiver);
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getRoutingKey() {
+		return routingKey;
+	}
+
+	public String getQueueName() {
+		return queueName;
+	}
+
+	public String getName() {
+		return name;
+	}
 
 	public MessageConverter jsonMessageConverter2() {
 		return new Jackson2JsonMessageConverter();
 	}
+
 	/*
-	 * 
+	 * Creates a queue for a specific operation if it is not present and binds the
+	 * counter to the queue.If a queue is already present for the specific operation
+	 * the counter is binded to the queue.
 	 */
-    private void initContainer(Receiver receiver) {
-        // set up the queue, exchange, binding on the broker
-        RabbitAdmin admin = new RabbitAdmin(connectionFactory);
-        Queue queue = new Queue(queueName);
-        admin.declareQueue(queue);
-        TopicExchange exchange = new TopicExchange("tokens-exchange");
-        admin.declareExchange(exchange);
-        admin.declareBinding(BindingBuilder.bind(queue).to(exchange).with(routingKey));
+	private void initContainer(Receiver receiver) {
+		// set up the queue, exchange, binding on the broker
+		RabbitAdmin admin = new RabbitAdmin(connectionFactory);
+		Queue queue = new Queue(queueName);
+		admin.declareQueue(queue);
+		TopicExchange exchange = new TopicExchange("tokens-exchange");
+		admin.declareExchange(exchange);
+		admin.declareBinding(BindingBuilder.bind(queue).to(exchange).with(routingKey));
 
-        // set up the listener and container
-        SimpleMessageListenerContainer container =
-                new SimpleMessageListenerContainer(connectionFactory);
+		// set up the listener and container
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory);
 
-        MessageListenerAdapter adapter = new MessageListenerAdapter(receiver, "receiveToken");
-        adapter.setMessageConverter(jsonMessageConverter2());
-        container.setMessageListener(adapter);
-        container.setQueueNames(queueName);
-        container.start();
-    }
+		MessageListenerAdapter adapter = new MessageListenerAdapter(receiver, "receiveToken");
+		adapter.setMessageConverter(jsonMessageConverter2());
+		container.setMessageListener(adapter);
+		container.setQueueNames(queueName);
+		container.start();
+	}
 
 }
